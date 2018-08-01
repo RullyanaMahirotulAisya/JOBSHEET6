46853a0eaded5a824c8f45997e0cba732b9aaa76
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-01 09:23:16 +0700

    Initial Commit

---
+++ src/JOBSHEET7/latihan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JOBSHEET7;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class latihan {
+    public static void main (String [] args) {
+        String identitas = "Rullyana Mahirotul Aisya / XRPL5 / 38";
+        System.out.println("Identitas : "+ identitas);
+        
+    }
+    
+}
