7ad1da23e1479741bf8026c7a4411ef3ad0151ed
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-02 21:49:04 +0700

    Initial Commit

---
+++ test/JOBSHEET6/ContohFormatString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JOBSHEET6;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ContohFormatString {
+    public static void main(String[] args) {
+        String namaDepan = "Andi";
+        String namaBelakang = "Bagus";
+        String namaAkhir = "Prasetya";
+        System.out.format("Nama saya %s %s %n", namaDepan, namaBelakang, namaAkhir);
+    }
+    
+}
